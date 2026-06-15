faff765943c287df8bfc5e06eccafcfa75458514
dxvina28-web <dxvina28@gmail.com>
2026-06-15 18:29:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
+const DEFAULT_KEY = "gsk_YOUR_NEW_KEY_HERE";
 
+const getGroqApiKey = () => {
+  const localKey = localStorage.getItem('groq_api_key');
+  if (localKey) return localKey;
+
+  localStorage.setItem('groq_api_key', DEFAULT_KEY);
+  return DEFAULT_KEY;
+};
+
+const GROQ_API_KEY = getGroqApiKey();
