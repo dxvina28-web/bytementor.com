fd25ede3c67f36b63e03a9ffd35d39c322f20250
dxvina28-web <dxvina28@gmail.com>
2026-06-15 17:39:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
+const DEFAULT_KEY = "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";
+
 const getGroqApiKey = () => {
   const localKey = localStorage.getItem('groq_api_key');
   if (localKey) return localKey;
-  return '';
+
+  localStorage.setItem('groq_api_key', DEFAULT_KEY);
+  return DEFAULT_KEY;
 };
 
 const GROQ_API_KEY = getGroqApiKey();
-
-function setGroqApiKey(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) {
-  localStorage.setItem('groq_api_key', key);
-  location.reload();
-}
